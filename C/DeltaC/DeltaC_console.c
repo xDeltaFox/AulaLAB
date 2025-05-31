80797480980a76d3e43f96871fc1ecf96470ef3a
xDeltaFox <israelbz25@gmail.com>
2025-05-31 00:02:23 -0300

    Adicinando comentários a função 'DeltaC_print'

--- C/DeltaC/DeltaC_console.c
+++ C/DeltaC/DeltaC_console.c
@@ -3,6 +3,8 @@
 
 #include "./DeltaC_console.h"
 
+// Imprime somente strings e concatena '\n' para pula a linha automaticamente.
+// Essa função não suporta impressão de variaveis e concatenação de formatos: '%d'.
 void DeltaC_print(char str[])
 {
     char resultado[strlen(str) + 1];
